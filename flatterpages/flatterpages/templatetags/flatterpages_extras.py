2996b2e288d1cebd97548505c97cf9c2c8605456
Jake Alstad <jake.alstad@gmail.com>
2013-06-21 12:41:55 -0500

    added template tag to parse templates from database

---
+++ flatterpages/flatterpages/templatetags/flatterpages_extras.py
@@ -0,0 +1,11 @@
+from django import template
+from django.template import Context, loader
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def render(value):
+	return loader.get_template_from_string(value).render(Context())
